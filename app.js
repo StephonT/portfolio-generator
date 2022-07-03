acfd29ab2c3a627bd36377633413ef3feb1b404e
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-03 14:17:12 -0400

    modularized code and generated webpage

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem =>
-      console.log(profileItem)
-    );
-  };
+const generatePage = require('./src/page-template.js');
 
-  printProfileData(profileDataArgs);
+// Holds the data arguments
+const profileDataArgs = process.argv.slice(2);
+//Defines the data (Data destructuring)
+const [name, github] = profileDataArgs;
+
+// generatePage function was here. Moved it to its own file to Modularize code. With that being said, I had to add module.export at bottom of page-template file and require at the top of this file to use generatePage function. Now I'm able to use functions from one module inside another!
+
+  
+  // function to create file
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
