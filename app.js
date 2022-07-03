cfabf3b2853d3a76071d366fb99988d405df2664
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-03 15:04:01 -0400

    create gitignore file and installed inquirer

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
 
-// Holds the data arguments
-const profileDataArgs = process.argv.slice(2);
-//Defines the data (Data destructuring)
-const [name, github] = profileDataArgs;
+// Defining fs so fs.writeFile can be used
+// const fs = require('fs');
+
+// Used require so that generatePage function in page-template.js can be used in this module
+// const generatePage = require('./src/page-template.js');
+
+// Variable created to hold the generatePage function. Placed it in fs.writeFile as a parameter so we aren't using a function directly.
+// const pageHTML = generatePage(name, github);
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 // generatePage function was here. Moved it to its own file to Modularize code. With that being said, I had to add module.export at bottom of page-template file and require at the top of this file to use generatePage function. Now I'm able to use functions from one module inside another!
 
   
   // function to create file
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+//   fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw new Error (err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
